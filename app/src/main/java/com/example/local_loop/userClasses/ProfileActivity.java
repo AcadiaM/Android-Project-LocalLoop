120aa9411193b63fb52e735e4480d742b279c416
Rami Mosleh <rmosl009@uottawa.ca>
2025-05-25 11:14:06 -0400

    UI

--- app/src/main/java/com/example/local_loop/userClasses/ProfileActivity.java
+++ app/src/main/java/com/example/local_loop/userClasses/ProfileActivity.java
@@ -1,11 +1,6 @@
-package com.example.local_loop;
+package com.example.local_loop.userClasses;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,24 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.local_loop.R;
+
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-    public void OnCreateButton(View view){
-        // Application Context and Activity
-        Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
-        startActivityForResult(intent,0);
-
-    }
-
 }
