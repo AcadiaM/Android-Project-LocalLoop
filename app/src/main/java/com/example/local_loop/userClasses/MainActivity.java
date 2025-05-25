7ae1f85cf3e980a27e29c27f423b60396642520d
Paul3003 <paule.heckman@gmail.com>
2025-05-25 11:51:04 -0400

    Moved MainActivity and fixed error of import.

--- app/src/main/java/com/example/local_loop/userClasses/MainActivity.java
+++ app/src/main/java/com/example/local_loop/userClasses/MainActivity.java
@@ -13,6 +13,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.local_loop.userClasses.ProfileActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
