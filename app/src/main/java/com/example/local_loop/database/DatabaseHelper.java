d148c84ed3e55994f969016ab046878c769eef6a
Paul3003 <paule.heckman@gmail.com>
2025-07-14 19:32:58 -0400

    Added javadoc comments to dbHelper functions.

--- app/src/main/java/com/example/local_loop/database/DatabaseHelper.java
+++ app/src/main/java/com/example/local_loop/database/DatabaseHelper.java
@@ -186,6 +186,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return null;
     }
 
+    /**
+     * This function finds the attendeeID of a user with it's username.
+     *
+     * @param username the username of the given user.
+     * @return the ID if the username is found in the database, -1 otherwise.
+     */
     public int getIDByUsername(String username) {
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.query(TABLE_USERS, new String[]{"id"}, "username = ?", new String[]{username}, null, null, null);
